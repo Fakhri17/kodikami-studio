af037c897d19563b63bc4496953a873786325492
Fakhri17 <fakhricoffe@gmail.com>
2025-03-18 10:10:21 +0700

    fix: dynamic meta title

--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,24 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: 'Comming Soon',
+        description: 'This is the home page',
+      },
     },
   ],
+})
+
+router.beforeEach((to) => {
+  const { title, description } = to.meta
+  const defaultTitle = 'Default Title'
+  const defaultDescription = 'Default Description'
+
+  document.title = title || defaultTitle
+
+  // const descriptionElement = document.querySelector('head meta[name="description"]')
+
+  // descriptionElement.setAttribute('content', description || defaultDescription)
 })
 
 export default router
